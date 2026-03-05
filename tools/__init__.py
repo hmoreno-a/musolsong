77de6bcd2c2c7ab446084d02cebdd1d5860b30ed
Heidy <hmoreno@iac.es>
2026-03-05 10:37:44 +0000

    Added Readme and init files for tools

---
+++ tools/__init__.py
@@ -0,0 +1,8 @@
+"""
+    tools/__init__.py
+    
+    This module contains tools for  MUSOLSONG Software System.
+    SearchLogs is a type of "grep" command use to analize logs. 
+    Author: hmoreno  
+"""
+__all__ = ["searchLogs.py" ]
